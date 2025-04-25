845fa77d66e7f8bb1bf78b7ffddddc0281c06129
pablo <pablomoralestenis@gmail.com>
2025-04-25 17:13:11 -0400

    Update validator.c

--- validator.c
+++ validator.c
@@ -1,1 +1,17 @@
+#include <stdlib.h>
+#include <ctype.h>
+#include "validator.h"
 
+int is_valid_seismic_level(const char *value) {
+    int num = atoi(value);
+    return num >= 1 && num <= 5;
+}
+
+int is_valid_risk_percent(const char *value) {
+    for (int i = 0; value[i]; i++) {
+        if (!(isdigit(value[i]) || value[i] == '.'))
+            return 0;
+    }
+    float num = atof(value);
+    return num >= 0.0 && num <= 100.0;
+} 
