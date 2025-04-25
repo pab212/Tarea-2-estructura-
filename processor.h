962bdb1b3d4a6863963af476f9d2bba366e074ff
pablo <pablomoralestenis@gmail.com>
2025-04-25 17:12:37 -0400

    Update processor.h

--- processor.h
+++ processor.h
@@ -1,1 +1,15 @@
+#ifndef PROCESSOR_H
+#define PROCESSOR_H
 
+typedef struct {
+    char city_name[50];
+    int seismic_level;
+    float risk_percent;
+    int missing_risk;
+} CityData;
+
+CityData* read_csv(const char *filename, int *count);
+void write_output(const char *filename, CityData *data, int n);
+
+#endif
+ 
